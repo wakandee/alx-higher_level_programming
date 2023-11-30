fcbae432eec397e8f95ae393e01a709b19f99a10
Allan Kipruto <koechallanhi@gmail.com>
2023-11-30 06:20:07 -0800

    finl changes

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
+from add_0 import add
 
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    print("<a {}> + <b {}> = <add(a,b) {}>".format(a,b,(a+b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

5405a65057377a6e222a8dbdd7c6f0979f01af18
Allan Kipruto <koechallanhi@gmail.com>
2023-12-03 01:54:11 -0800

    changes 2

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
     x  = len(my_list) -1
-    for i in range(x):
-        print("{}".format(my_list[i]))
+    while x >= 0:
+        print("{}".format(my_list[x]))

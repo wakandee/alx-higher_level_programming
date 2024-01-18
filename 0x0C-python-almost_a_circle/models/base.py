a7e88b59c85aaca1aa334aa6da764cfe2004bf51
Allan Kipruto <koechallanhi@gmail.com>
2024-01-18 21:20:51 +0300

    Base updated 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
 class Base:
-	""" Base """
-	__nb_objects = 0
+    """ Base """
+    __nb_objects = 0
 
-	def __init__(self, id=12):
-		if id is not None:
-			Base.id = id
-		else:
-			Base.__nb_objects += 1
-			Base.id =__nb_objects
-		
-		md
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects

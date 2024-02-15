33deedbb03d35f6a97121ab661ae8d05376a468d
Allan Kipruto <koechallanhi@gmail.com>
2024-02-15 16:28:39 +0300

    create table without errors

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Write a script that creates a table called first_table in the current database in your MySQL server.
+
+-- first_table description:
+-- id INT
+-- name VARCHAR(256)
+
+CREATE table if NOT EXISTS first_table (id INT,name VARCHAR (256));

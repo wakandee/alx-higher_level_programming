777b9cde4aca7c11f2a469e58367bca528ed8351
Allan Kipruto <koechallanhi@gmail.com>
2024-04-02 01:12:32 +0300

    python network

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,8 +7,7 @@ from sys import argv
 if __name__ == '__main__':
 
     # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],passwd=argv[2], db=argv[3])
 
     # It gives us the ability to have multiple seperate working environments
     # through the same connection to the database.
